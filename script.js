8c12e7453ad36d118463e6c3d044c961c15c7869
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2024-06-23 12:29:58 +0530

    empty an array

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// 1 - Given a string, reverse each word in the sentence.
+// Question no:- 1 - Given a string, reverse each word in the sentence.
 
-var str = "seere jaha se achha hindustan humara";
+// var str = "seere jaha se achha hindustan humara";
 
-var savedStrArray = str.split(" ").map(function (word){
-  return word.split("").reverse().join("")
-});
+// var savedStrArray = str.split(" ").map(function (word){
+//   return word.split("").reverse().join("")
+// });
 
-console.log(savedStrArray.join(" "));
+// console.log(savedStrArray.join(" "));
+
+
+//  Question no:- 2- How to check if an object is an array or not? Provide some code.
+
+// function cheackArray(elem){
+//   return Array.isArray(elem)
+// }
+// console.log(cheackArray([]));
+// console.log(cheackArray({}));
+
+//  Question no:- 3- How to empty an array in JavaScript? do not reset it to a new array, and do not loop through to pop each value
+
+// var arr = [1, 2, 3, 4, 5, 6,]
+// arr.length = 0;
